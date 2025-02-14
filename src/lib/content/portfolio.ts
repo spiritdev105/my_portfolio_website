4813e7a7e77e9537d5fdc669abf6093f23b5e366
Koni04123 <jordan.ramirez0202@proton.me>
2025-02-14 05:45:00 -0500

    init

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,39 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/vatsalsinghkv',
+  instagram: 'https://www.instagram.com/vatsalsinghkv',
+  twitter: 'https://twitter.com/vatsalsinghkv',
+  github: 'https://github.com/vatsalsinghkv',
+  linkedin: 'https://www.linkedin.com/in/vatsalsinghkv/',
+};
+
+export const author = {
+  name: 'Artem Savenko',
+  email: 'artemsavenko1717@gmail.com',
+};
+
+export const seoData = {
+  title: 'Artem Savenko | Front-End Developer',
+  description:
+    'Artem Savenko is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://vatsalsinghkv.vercel.app/',
+  keywords: [
+    'Vatsal',
+    'Artem Savenko',
+    '@vatsalsinghkv',
+    'vatsalsinghkv',
+    'Portfolio',
+    'Vatsal Portfolio ',
+    'Artem Savenko Portfolio',
+  ],
+};

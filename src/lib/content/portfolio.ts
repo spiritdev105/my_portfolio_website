1c9c888f56bcaa9eeb5044ab5cda497b404d58e0
mickey <kelsey.abreu6767@gmail.com>
2025-02-17 15:48:01 -0500

    chore: update author information and social links in portfolio

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -10,13 +10,13 @@ export const socialLinks: StringKeyValueType = {
   facebook: 'https://www.facebook.com/vatsalsinghkv',
   instagram: 'https://www.instagram.com/vatsalsinghkv',
   twitter: 'https://twitter.com/vatsalsinghkv',
-  github: 'https://github.com/vatsalsinghkv',
+  github: 'https://github.com/olaf11071107',
   linkedin: 'https://www.linkedin.com/in/vatsalsinghkv/',
 };
 
 export const author = {
   name: 'Jonathan MacLeod',
-  email: 'dawncoding1107@gmail.com',
+  email: 'master@olafdev.site',
 };
 
 export const seoData = {
@@ -24,16 +24,16 @@ export const seoData = {
   description:
     'Jonathan MacLeod is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
   author: author.name,
-  image:
-    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
-  url: 'https://vatsalsinghkv.vercel.app/',
+  // image:
+  //   'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://olafdev.site/',
   keywords: [
-    'Vatsal',
+    'OlafDev',
     'Jonathan MacLeod',
-    '@vatsalsinghkv',
-    'vatsalsinghkv',
+    '@olaf11071107',
+    'olafdev.site',
     'Portfolio',
-    'Vatsal Portfolio ',
+    'Jonathan MacLeod ',
     'Jonathan MacLeod Portfolio',
   ],
 };

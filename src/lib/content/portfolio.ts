48d0817836509a72bba2c236c3f6fb1ae7c52b94
mickey <kelsey.abreu6767@gmail.com>
2025-02-19 14:00:46 -0500

    feat: integrate Supabase for log file uploads and update portfolio URL

--- src/lib/content/portfolio.ts
+++ src/lib/content/portfolio.ts
@@ -26,7 +26,7 @@ export const seoData = {
   author: author.name,
   image:
     'https://raw.githubusercontent.com/Mickey9315/jonathan-portfolio/refs/heads/main/Screenshot%202025-02-17%20155256.png?token=GHSAT0AAAAAAC5TEQLSEYWFR2RY47M37PP4Z5TUNBQ',
-  url: 'https://olafdev.site/',
+  url: 'https://portfolio.olafdev.site/',
   keywords: [
     'OlafDev',
     'Jonathan MacLeod',
